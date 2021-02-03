e539bc11fc44eff4efd4cd6eea5aab14b98f0eeb
Raimondas Rimkevičius <github@mekdrop.name>
2021-02-03 11:26:31 +0200

    Initcial code

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# smarty-includeq
-Rewritten @smarty-php include variant that was invented for use in @XOOPS, but nowadays is used in some other PHP based CMS'es
+[![License](https://img.shields.io/github/license/imponeer/smarty-includeq.svg)](LICENSE)
+[![GitHub release](https://img.shields.io/github/release/imponeer/smarty-includeq.svg)](https://github.com/imponeer/smarty-includeq/releases) [![Maintainability](https://api.codeclimate.com/v1/badges/05e38f936681d6b4c462/maintainability)](https://codeclimate.com/github/imponeer/smarty-includeq/maintainability) [![PHP](https://img.shields.io/packagist/php-v/imponeer/smarty-includeq.svg)](http://php.net) 
+[![Packagist](https://img.shields.io/packagist/dm/imponeer/smarty-includeq.svg)](https://packagist.org/packages/imponeer/smarty-includeq)
+
+# Smarty IncludeQ
+
+Rewritten (due that original use GPLv2+ license) [Smarty](https://smarty.net) '[include](https://www.smarty.net/docsv2/en/language.function.include.tpl)' variant that was invented for use in [XOOPS](https://xoops.org), but nowadays used in some other PHP based CMS'es (like [ImpressCMS](https://impresscms.org)!).
+
+See, [original version of this smarty plugin in Xoops](https://github.com/XOOPS/XoopsCore25/blob/v2.5.8/htdocs/class/smarty/xoops_plugins/compiler.includeq.php) to see more accurate description why this plugin exists.
+
+## Installation
+
+To install and use this package, we recommend to use [Composer](https://getcomposer.org):
+
+```bash
+composer require imponeer/smarty-includeq
+```
+
+Otherwise, you need to include manually files from `src/` directory. 
+
+## Registering in Smarty
+
+If you want to use these extensions from this package in your project you need register them with [`registerPlugin` function](https://www.smarty.net/docs/en/api.register.plugin.tpl) from [Smarty](https://www.smarty.net). For example:
+```php
+$smarty = new \Smarty();
+$includeqPlugin = new \Imponeer\Smarty\Extensions\IncludeQ\IncludeQCompiler();
+$smarty->registerPlugin('compiler', $includeqPlugin->getName(), [$includeqPlugin, 'execute']);
+```
+
+## Using from templates
+
+Example how to use it:
+```smarty
+  {includeq file="file.tpl"}
+```
+## How to contribute?
+
+If you want to add some functionality or fix bugs, you can fork, change and create pull request. If you not sure how this works, try [interactive GitHub tutorial](https://try.github.io).
+
+If you found any bug or have some questions, use [issues tab](https://github.com/imponeer/smarty-includeq/issues) and write there your questions.
---
+++ src/IncludeQCompiler.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Imponeer\Smarty\Extensions\IncludeQ;
+
+use Imponeer\Contracts\Smarty\Extension\SmartyCompilerInterface;
+use Smarty_Internal_SmartyTemplateCompiler;
+use SmartyCompilerException;
+
+/**
+ * Defines {includeq} smarty tag
+ *
+ * @package Imponeer\Smarty\Extensions\IncludeQ
+ */
+class IncludeQCompiler implements SmartyCompilerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function execute($args, Smarty_Internal_SmartyTemplateCompiler &$compiler)
+    {
+        $this->validateArguments($args, $compiler);
+
+        $ret = '';
+
+        if (isset($args['assign'])) {
+            $ret .= "ob_start();\n";
+        }
+
+        $ret .= sprintf(
+            '$_smarty_tpl->_subTemplateRender(%s, $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, %s, 0, true);',
+            $args['file'],
+            $this->renderOtherArgs($args)
+        );
+
+        if (isset($args['assign'])) {
+            $ret .= sprintf(
+                '$_smarty_tpl->assign(%s, ob_get_contents()); ob_end_clean();',
+                $args['assign']
+            );
+        }
+
+        return '<?php ' . $ret . ' ?>';
+    }
+
+    /**
+     * Validates tag arguments
+     *
+     * @param array $args Arguments supplied for tag
+     * @param Smarty_Internal_SmartyTemplateCompiler $compiler Current smarty compiler instance
+     *
+     * @throws SmartyCompilerException
+     */
+    protected function validateArguments(array $args, Smarty_Internal_SmartyTemplateCompiler $compiler)
+    {
+        if (!isset($args['file'])) {
+            $compiler->trigger_template_error(
+                'includeq must have "file" attribute',
+                null,
+                true
+            );
+        }
+
+        if (empty($args['file'])) {
+            $compiler->trigger_template_error(
+                'includeq must have non empty "file" attribute',
+                null,
+                true
+            );
+        }
+
+        if (isset($args['assign']) && !$this->isVariableName($args['assign'])) {
+            $compiler->trigger_template_error(
+                'includeq "assign" attribute must be variable name',
+                null,
+                true
+            );
+        }
+    }
+
+    /**
+     * Checks if argument can be variable name
+     *
+     * @param string $arg Argument name
+     *
+     * @return bool
+     */
+    private function isVariableName(string $arg): bool
+    {
+        return (bool)preg_match('/^\w+$/', $arg);
+    }
+
+    /**
+     * Renders other arguments as string
+     *
+     * @param array $args All supplied tag arguments
+     *
+     * @return string
+     */
+    protected function renderOtherArgs(array $args): string
+    {
+        $ret = '[';
+
+        foreach ($this->getOtherArguments($args) as $name => $value) {
+            $ret .= sprintf(
+                "'%s' => %s,",
+                $name,
+                is_bool($value) ? var_export($value, true) : $value
+            );
+        }
+
+        return $ret . ']';
+    }
+
+    /**
+     * Gets arguments that doesn't have any specific logic for usability
+     *
+     * @param array $args Supplied for tag arguments
+     *
+     * @return array
+     */
+    protected function getOtherArguments(array $args): array
+    {
+        $ret = array_merge($args);
+
+        foreach (['file', 'assign'] as $arg) {
+            if (isset($ret[$arg])) {
+                unset($ret[$arg]);
+            }
+        }
+
+        return $ret;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return 'includeq';
+    }
+}
